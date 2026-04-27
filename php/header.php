2b1d8d1478d07a7ba966ef217e18f23c7ea65982
a25josmonces <a25josmonces@inspedralbes.cat>
2026-04-27 10:30:01 +0200

    Header i footers bàsics.

--- php/header.php
+++ php/header.php
@@ -4,10 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD de PHP con MySQLi by parzibyte</title>
+    <title>Gestió d'incidències.</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 
 <body>
 <main class="container-fluid">
+
+    <header>
+        Registre d'incidències.
+    </header>
 
