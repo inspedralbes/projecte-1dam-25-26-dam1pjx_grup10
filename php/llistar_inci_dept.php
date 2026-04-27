8f632820a1da24a57e577e95eb394d538c7ccaef
a25josmonces <a25josmonces@inspedralbes.cat>
2026-04-27 14:26:20 +0200

    Començat a fer llistar incidències a usuari segons departament

---
+++ php/llistar_inci_dept.php
@@ -0,0 +1,39 @@
+<?php include_once "header.php"; ?>
+<?php
+$mysqli = require_once 'connexio.php';
+$departament = intval($_POST["departament"]);
+
+$sentencia = $mysqli->prepare("SELECT * FROM INCIDENCIA WHERE departament = ?");
+$sentencia->bind_param("i", $departament);
+$sentencia->execute();
+
+$resultat = $sentencia->get_result();
+$dades = [];
+
+while ($fila = $resultat->fetch_assoc()) {
+    $dades[] = $fila;
+}
+?>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th>Actuació</th>
+            <th>Descripció</th>
+            <th>Temps trigat</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $comptador = 1; ?>
+        <?php foreach ($dades as $fila): ?>
+        <tr>
+            <td>Actuació <?= $comptador ?></td>
+            <td><?= $fila["descripcio"] ?></td>
+            <td><?= $fila["temps"] ?></td>
+        </tr>
+        <?php $comptador++; ?>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<?php include_once "footer.php"; ?>
