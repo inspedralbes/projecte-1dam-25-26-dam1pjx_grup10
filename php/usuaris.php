d6d1e9ab7eb3e0ea94f05ba8d8281cd94fd2c31b
a25izagomdel <a25izagomdel@inspedralbes.cat>
2026-04-27 12:23:20 +0200

    Fer a la pagina de usuari el trobar incidencia, falta probar un avegada estigui fet el llistar_inci.php

---
+++ php/usuaris.php
@@ -0,0 +1,49 @@
+<?php include_once "header.php"; ?>
+<div class="row">
+    <div class="col-12">
+        <h1>Registar Incidencia</h1>
+        <form action="registrar_inci.php" method="POST">
+            <div class="form-group">
+                <label for="departament">Departament</label>
+                <select name="departament" id="departament" required>
+                    <option value="" selected disabled>-- Selecciona departament --</option>
+                    <option value="1">Informàtica</option>
+                    <option value="2">Català</option>
+                    <option value="3">Castellá</option>
+                    <option value="4">Matemàtiques</Matemàti></option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="descripcio">Descripción</label>
+                <textarea placeholder="Descripció" class="form-control" name="descripcio" id="descripcio" cols="30" rows="10" required></textarea>
+            </div>
+            <div class="form-group"><button class="btn btn-success">Guardar</button></div>
+        </form>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <h1>Trobar Incidencia</h1>
+        <form action="llistar_inci.php" method="POST">
+            <div class="form-group">
+                <label for="departament">Departament</label>
+                <select name="departament" id="departament">
+                    <option value="" disabled selected>-- Selecciona departament --</option>
+                    <option value="1">Informàtica</option>
+                    <option value="2">Català</option>
+                    <option value="3">Castellá</option>
+                    <option value="4">Matemàtiques</Matemàti></option>
+                </select>
+            </div>
+
+            <p>O</p>
+
+            <div class="form-group">
+                <label for="idIncidencia">Numero Incidencia</label>
+                <textarea name="idIncidencia" placeholder="Indica el numero de Incidencia"></textarea>
+            </div>
+            <div class="form-group"><button class="btn btn-success">Trobar</button></div>
+        </form>
+    </div>
+</div>
+<?php include_once "footer.php"; ?>
