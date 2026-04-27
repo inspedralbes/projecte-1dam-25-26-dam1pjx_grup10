7369ad1b1436f61d9444421f8495bc83a1afd49c
a25josmonces <a25josmonces@inspedralbes.cat>
2026-04-27 10:57:48 +0200

    Afegit botó retorn al footer

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,6 @@
 </main>
 <footer class="px-2 py-2 fixed-bottom bg-dark">
+    <a href = "../"> Tornar </a>
         <span class="text-muted">
             Aplicació de gestió d'incidències informàtiques. &nbsp;|&nbsp; Desenvolupat per Izan i Josep.
         </span>
