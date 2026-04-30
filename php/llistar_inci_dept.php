b1be6b68f5c6d18a42229272c18cf9196c3c4e67
a25izagomdel <a25izagomdel@inspedralbes.cat>
2026-04-30 10:45:40 +0200

    Fer que al llitsra incidencia per departament surti l'estat de l'incidencia

--- php/llistar_inci_dept.php
+++ php/llistar_inci_dept.php
@@ -15,27 +15,35 @@ while ($fila = $resultat->fetch_assoc()) {
 }
 ?>
 
-<table class="table">
-    <thead>
+    <table class="table">
+        <thead>
         <tr>
             <th>ID Incidència</th>
             <th>Descripció</th>
             <th>Data inici</th>
+            <th>Estat</th>
             <th></th>
         </tr>
-    </thead>
-    <tbody>
+        </thead>
+        <tbody>
         <?php $comptador = 1; ?>
         <?php foreach ($dades as $fila): ?>
-        <tr>
-            <td><?= $fila["idIncidencia"] ?></td>
-            <td><?= $fila["descripcio"] ?></td>
-            <td><?= $fila["data_inici"] ?></td>
-            <td> <a href="llistar_inci_id.php?idIncidencia=<?php echo $fila["idIncidencia"]; ?>"> Veure</a></td>
-        </tr>
-        <?php $comptador++; ?>
+            <tr>
+                <td><?= $fila["idIncidencia"] ?></td>
+                <td><?= $fila["descripcio"] ?></td>
+                <td><?= $fila["data_inici"] ?></td>
+                <td>
+                    <?php if (empty($fila["data_fi"]) || is_null($dades[0]["data_fi"])): ?>
+                        <p>Pendent</p>
+                    <?php else: ?>
+                        <p>Resolta</p>
+                    <?php endif; ?>
+                </td>
+                <td> <a href="llistar_inci_id.php?idIncidencia=<?php echo $fila["idIncidencia"]; ?>"> Veure</a></td>
+            </tr>
+            <?php $comptador++; ?>
         <?php endforeach; ?>
-    </tbody>
-</table>
+        </tbody>
+    </table>
 
 <?php include_once "footer.php"; ?>
