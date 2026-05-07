626c26b88bb42b7541a2838835b57ac7b6b965c7
a25izagomdel <a25izagomdel@inspedralbes.cat>
2026-05-07 08:11:11 +0200

    eliminar connexio_mongo.php de github, vaig pusar-ho per error

--- php/js/connexio_mongo.php
+++ php/js/connexio_mongo.php
@@ -1,9 +1,1 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
-$uri = getenv('mongodb+srv://projecte-1dam-25-26-dam1pj10_user:mPG10BD_s@projecte-1dam-25-26-dam.u52motu.mongodb.net/') ?: throw new RuntimeException(
-    'Set the MONGODB_URI environment variable to your Atlas URI',
-);
-$client = new MongoDB\Client($uri);
-$collection = $client->LOGS->LOGS;
-$results = $collection->insertOne(url: ["google.es"], metode: "post", usuari_id: "Usuari", data_inici_sessio: new Date(), navegador: "Firefox", ip: "192.168.123.54", temps_resposta_ms: 123);
 
