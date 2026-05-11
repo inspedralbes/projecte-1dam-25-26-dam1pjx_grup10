f9ab45f44ec10ae72d9f97344ac5a7c53b282255
a25josmonces <a25josmonces@inspedralbes.cat>
2026-05-11 12:34:30 +0200

    Implementat enviament de correu al registrar-se una incidència

--- php/registrar_inci.php
+++ php/registrar_inci.php
@@ -12,6 +12,10 @@ $sentencia->bind_param("is", $departament, $descripcio);
 $sentencia->execute();
 
 $ultimID = $mysqli->insert_id;
+
+mail('a25josmonces@inspedralbes.cat, a25izagomdel@inspedralbes.cat', 'Nova incidència registrada', 'S\'ha registrat una nova incidència
+del departament de ' $_POST['departament'] '\n' 'Descripció: \n ' $_POST['descripcio']);
+
 ?>
 
 <div class="container-mitja text-center">
@@ -19,6 +23,5 @@ $ultimID = $mysqli->insert_id;
 <p>L'ID de la teva incidència és <?php echo $ultimID ?> </p>
 <a href="./index.php" class="btn btn-outline-primary">Finalitzar</a>
 </div>
-<?php include_once "correu.php" ;?>
 <?php $link = 'usuaris.php'; ?>
 <?php include_once "footer.php"; ?>
