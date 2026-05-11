d48e6d009e454348e2709f5671d98e4fbb7ddbaf
a25izagomdel <a25izagomdel@inspedralbes.cat>
2026-05-11 11:45:28 +0200

    Fer grafic de estadistiques.php de mongoDB amb mongoDB Charts per les tres estadistiques posades

--- php/estadistiques.php
+++ php/estadistiques.php
@@ -28,12 +28,35 @@ $pipeline_dias = [
     ]
 ];
 $resultados = $collection->aggregate($pipeline_dias);
-foreach ($resultados as $document) {
-    echo "Dia: " . $document->data_inici_sessio ."<br>" . "Total: " . $document->total . "<br>";
-}
-echo "</div>";
+?>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Data</th>
+        <th>Visites</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($resultados as $document): ?>
+        <tr>
+            <td><?= $document["data_inici_sessio"] ?></td>
+            <td><?= $document["total"] ?></td>
+        </tr>
+
+    <?php endforeach; ?>
+    </tbody>
+</table>
+</div>
+<div class='container'>
+    <h2>Accessos per dia</h2>
+    <iframe class="container" style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"
+            width="640"
+            height="480"
+            src="https://charts.mongodb.com/charts-project-0-japdbiq/embed/charts?id=f2f7e522-5af0-4577-8734-7212448b77dc&maxDataAge=14400&theme=light&autoRefresh=true"></iframe>
+</div>
+<?php
 echo "<div class='container'>\n";
-echo "<h2> Pagines mes visitades</h2>\n";
+echo "<h2> Pàgines més visitades</h2>\n";
 $pipeline_pagmesvisit = [
     [
         '$group' => [
@@ -51,17 +74,54 @@ $pipeline_pagmesvisit = [
     [
         '$project' => [
             '_id' => 0,
-            'url' => '$_id',
-            'total' => 1]
+            'url' => [
+                '$replaceOne' => [
+                    'input' => [
+                        '$arrayElemAt' => [
+                            ['$split' => ['$_id', '/']],
+                            -1
+                        ]
+                    ],
+                    'find' => '.php',
+                    'replacement' => ''
+                ]
+            ],
+            'total' => 1
+        ]
     ]
 ];
 $resultados = $collection->aggregate($pipeline_pagmesvisit);
-foreach ($resultados as $document) {
-    echo "URL: " . $document->url ."<br>" . "Total: " . $document->total . "<br>";
-}
-echo "</div>";
+?>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Pagina</th>
+        <th>Visites</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($resultados as $document): ?>
+        <tr>
+            <td><?= $document["url"] ?></td>
+            <td><?= $document["total"] ?></td>
+        </tr>
+
+    <?php endforeach; ?>
+    </tbody>
+</table>
+</div>
+<div class='container' style="text-align: center;">
+    <h2>Pàgines més Visitades</h2>
+    <iframe
+        class="container"
+        style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"
+        width="640"
+        height="480"
+        src="https://charts.mongodb.com/charts-project-0-japdbiq/embed/charts?id=030043c8-a08d-453c-a09a-2da8143184ca&maxDataAge=14400&theme=light&autoRefresh=true"></iframe>
+</div>
+<?php
 echo "<div class='container'>\n";
-echo "<h2> Usuaris mes actius</h2>\n";
+echo "<h2> Usuaris més actius</h2>\n";
 $pipeline_usumesact = [
     [
         '$group' => [
@@ -86,10 +146,37 @@ $pipeline_usumesact = [
     ]
 ];
 $resultados = $collection->aggregate($pipeline_usumesact);
-foreach ($resultados as $document) {
-    echo "IP: " . $document->ip ."<br>" . "Total: " . $document->total . "<br>";
-}
-echo "</div>";
+?>
+<table class="table">
+    <thead>
+    <tr>
+        <th>IP</th>
+        <th>Total Visites</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($resultados as $document): ?>
+        <tr>
+            <td><?php echo $document->ip; ?></td>
+            <td><?= $document["total"] ?></td>
+        </tr>
+
+    <?php endforeach; ?>
+    </tbody>
+</table>
+</div>
+<div class='container' style="text-align: center;">
+    <h2>Usuaris mes actius</h2>
+    <iframe
+        class="container"
+        style="background: #FFFFFF;border: none;border-radius:2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"
+        width="640"
+        height="480"
+        src="https://charts.mongodb.com/charts-project-0-japdbiq/embed/charts?id=3800cfea-d34d-49a0-a146-fee6a8208098&maxDataAge=14400&theme=light&autoRefresh=true">
+    </iframe>
+</div>
+
+<?php
 echo "<div class='container'>\n";
 echo "<h2> Accessos Totals</h2>\n";
 $pipeline_totalaccess = [
@@ -100,7 +187,7 @@ $pipeline_totalaccess = [
     ],
     [
         '$group' => [
-            '_id' => '$url',
+            '_id' => null,
             'total' => ['$sum' => 1]
         ]
     ],
@@ -112,10 +199,23 @@ $pipeline_totalaccess = [
     ]
 ];
 $resultados = $collection->aggregate($pipeline_totalaccess);
-foreach ($resultados as $document) {
-    echo "Total accesss: " . $document->total ."<br>";
-}
-
 ?>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Total Access</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($resultados as $document): ?>
+        <tr>
+            <td><?= $document["total"] ?></td>
+        </tr>
 
+    <?php endforeach; ?>
+    </tbody>
+</table>
+</div>
+
+<?php $link = 'responsables.php' ?>
 <?php include_once "footer.php"?>
