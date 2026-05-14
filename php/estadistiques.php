69678ef4d2a385f8aad699cce0ef9d5ca16ad438
a25izagomdel <a25izagomdel@inspedralbes.cat>
2026-05-14 08:44:16 +0200

    Arreglar perquè funcioni l'apartat d'estadistiques de pàgines més visitades, perquè funcioni en produccio

--- php/estadistiques.php
+++ php/estadistiques.php
@@ -135,37 +135,36 @@ $resultados = $collection->aggregate($pipeline_dias);
 echo "<div class='container'>\n";
 echo "<h2> Pàgines més visitades</h2>\n";
 $pipeline_pagmesvisit = [
-    [
-        '$group' => [
-            '_id' => '$url',
-            'total' => ['$sum' => 1]
-        ]
-    ],
-    [
-        '$sort' => [
-            'total' => -1]
-    ],
-    [
-        '$limit' => 5
-    ],
-    [
-        '$project' => [
-            '_id' => 0,
-            'url' => [
-                '$replaceOne' => [
-                    'input' => [
-                        '$arrayElemAt' => [
-                            ['$split' => ['$_id', '/']],
-                            -1
-                        ]
-                    ],
-                    'find' => '.php',
-                    'replacement' => ''
+        [
+                '$group' => [
+                        '_id' => '$url',
+                        'total' => ['$sum' => 1]
                 ]
-            ],
-            'total' => 1
+        ],
+        [
+                '$sort' => ['total' => -1]
+        ],
+        [
+                '$limit' => 5
+        ],
+        [
+                '$project' => [
+                        '_id' => 0,
+                        'url' => [
+                                '$replaceOne' => [
+                                        'input' => [
+                                                '$arrayElemAt' => [
+                                                        ['$split' => ['$_id', '/']],
+                                                        -1
+                                                ]
+                                        ],
+                                        'find' => '.php',
+                                        'replacement' => ''
+                                ]
+                        ],
+                        'total' => 1
+                ]
         ]
-    ]
 ];
 $resultados = $collection->aggregate($pipeline_pagmesvisit);
 ?>
